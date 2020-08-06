23f73bf967a498679d701238a189b38abba0d100
Elijah Kirschen <ekirschen584@gmail.com>
2020-08-06 22:35:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  # Write your code here
+  get '/reversename/:name' do
+    
+  
+  get '/square/:number' do
+    
+  
 
 end
